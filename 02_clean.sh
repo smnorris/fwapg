6780fa3ab3742049979eea9e3aac9af442536587
Simon Norris <snorris@hillcrestgeo.ca>
2019-06-27 08:49:24 -0700

    add missing queries to control script

--- 02_clean.sh
+++ 02_clean.sh
@@ -11,12 +11,19 @@ psql -f sql/create_fwa_named_streams.sql
 psql -f sql/create_fwa_waterbodies.sql
 psql -f sql/create_fwa_watershed_groups_subdivided.sql
 
-# add functions
+# linear functions
 psql -f sql/fwa_upstreamwsc.sql
 psql -f sql/fwa_lengthdownstream.sql
-psql -f sql/fwa_lengthupstream.sql
 psql -f sql/fwa_lengthinstream.sql
-psql -f sql/fwa_slicewatershedatpoint.sql
+psql -f sql/fwa_lengthupstream.sql
+
+# watershed functions
 psql -f sql/create_fwa_approx_borders.sql
+psql -f sql/CDB_MakeHexagon.sql
+psql -f sql/fwa_upstreambordercrossings.sql
+psql -f sql/fwa_slicewatershedatpoint.sql
+
 psql -f sql/fwa_watershedexbc.sql
-psql -f sql/fwa_refinedwatershed.sql
+psql -f sql/fwa_watershedrefined.sql
+psql -f sql/fwa_watershedhex.sql
+psql -f sql/fwa_watershedstream.sql
