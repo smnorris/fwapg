10b4d9f16f04624b03bca5e08767b56898fe61c3
Simon Norris <snorris@hillcrestgeo.ca>
2020-06-19 11:18:42 -0700

    basin_name is probably a better descriptor

--- sql/create_fwa_basins_poly.sql
+++ sql/create_fwa_basins_poly.sql
@@ -10,15 +10,15 @@ DROP TABLE IF EXISTS whse_basemapping.fwa_basins_poly;
 
 CREATE TABLE whse_basemapping.fwa_basins_poly
 (watershed_cache_id serial primary key,
- name text,
+ basin_name text,
  wscode_ltree ltree,
  localcode_ltree ltree,
  geom Geometry);
 
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'Thompson River' as name,
+ 'Thompson River' as basin_name,
 '100.190442'::ltree as wscode_ltree,
 '100.190442'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -26,12 +26,12 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '100.190442'::ltree AND
  localcode_ltree <@ '100.190442'::ltree
-GROUP BY name, '100.190442'::ltree, '100.190442'::ltree;
+GROUP BY basin_name, '100.190442'::ltree, '100.190442'::ltree;
 
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'Chilcotin River' as name,
+ 'Chilcotin River' as basin_name,
 '100.342455'::ltree as wscode_ltree,
 '100.342455'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -39,11 +39,11 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '100.342455'::ltree AND
  localcode_ltree <@ '100.342455'::ltree
- GROUP BY name, '100.342455'::ltree, '100.342455'::ltree;
+ GROUP BY basin_name, '100.342455'::ltree, '100.342455'::ltree;
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'Quesnel River' as name,
+ 'Quesnel River' as basin_name,
 '100.458399'::ltree as wscode_ltree,
 '100.458399'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -51,12 +51,12 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '100.458399'::ltree AND
  localcode_ltree <@ '100.458399'::ltree
- GROUP BY name, '100.458399'::ltree, '100.458399'::ltree;
+ GROUP BY basin_name, '100.458399'::ltree, '100.458399'::ltree;
 
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'Blackwater River' as name,
+ 'Blackwater River' as basin_name,
 '100.500560'::ltree as wscode_ltree,
 '100.500560'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -64,12 +64,12 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '100.500560'::ltree AND
  localcode_ltree <@ '100.500560'::ltree
- GROUP BY name, '100.500560'::ltree, '100.500560'::ltree;
+ GROUP BY basin_name, '100.500560'::ltree, '100.500560'::ltree;
 
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'Chilako River' as name,
+ 'Chilako River' as basin_name,
 '100.567134'::ltree as wscode_ltree,
 '100.567134'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -77,12 +77,12 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '100.567134'::ltree AND
  localcode_ltree <@ '100.567134'::ltree
- GROUP BY name, '100.567134'::ltree, '100.567134'::ltree;
+ GROUP BY basin_name, '100.567134'::ltree, '100.567134'::ltree;
 
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'Salmon River' as name,
+ 'Salmon River' as basin_name,
 '100.591289'::ltree as wscode_ltree,
 '100.591289'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -90,12 +90,12 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '100.591289'::ltree AND
  localcode_ltree <@ '100.591289'::ltree
- GROUP BY name, '100.591289'::ltree, '100.591289'::ltree;
+ GROUP BY basin_name, '100.591289'::ltree, '100.591289'::ltree;
 
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'McGregor River' as name,
+ 'McGregor River' as basin_name,
 '100.639480'::ltree as wscode_ltree,
 '100.639480'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -103,14 +103,14 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '100.639480'::ltree AND
  localcode_ltree <@ '100.639480'::ltree
- GROUP BY name, '100.639480'::ltree, '100.639480'::ltree;
+ GROUP BY basin_name, '100.639480'::ltree, '100.639480'::ltree;
 
 
 
 INSERT INTO whse_basemapping.fwa_basins_poly
-(name, wscode_ltree, localcode_ltree, geom)
+(basin_name, wscode_ltree, localcode_ltree, geom)
 SELECT
- 'Kootenay River' as name,
+ 'Kootenay River' as basin_name,
 '300.625474'::ltree as wscode_ltree,
 '300.625474'::ltree as localcode_ltree,
  ST_Union(geom) as geom
@@ -118,7 +118,7 @@ FROM whse_basemapping.fwa_watershed_groups_poly
 WHERE
  wscode_ltree <@ '300.625474' AND
  localcode_ltree <@ '300.625474'::ltree
-GROUP BY name, '300.625474'::ltree, '300.625474'::ltree;
+GROUP BY basin_name, '300.625474'::ltree, '300.625474'::ltree;
 
 -- we want to be able to quickly relate these back to watershed groups
 ALTER TABLE whse_basemapping.fwa_watershed_groups_poly ADD COLUMN IF NOT EXISTS basin_id integer;
