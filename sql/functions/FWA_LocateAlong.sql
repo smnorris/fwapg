97f726158bb41859a15a66a205089f435b65605b
Simon Norris <snorris@hillcrestgeo.ca>
2020-09-03 15:18:35 -0700

    pg_featureserv funcs must return a table/setof, fix locatealong

--- sql/functions/FWA_LocateAlong.sql
+++ sql/functions/FWA_LocateAlong.sql
@@ -3,32 +3,32 @@
 DROP FUNCTION postgisftw.FWA_LocateAlong;
 CREATE OR REPLACE FUNCTION postgisftw.FWA_LocateAlong(blue_line_key integer, downstream_route_measure float)
 
-RETURNS geometry
-
+RETURNS TABLE (
+    geom                     geometry(Point, 3005)
+)
 
 AS
 
 $$
 
-declare
+DECLARE
    v_blkey    integer := blue_line_key;
    v_measure  float   := downstream_route_measure;
    v_geom     geometry;
-begin
 
+BEGIN
+
+RETURN QUERY
 SELECT
   (ST_Dump(ST_LocateAlong(s.geom, v_measure))).geom as geom
 FROM whse_basemapping.fwa_stream_networks_sp AS s
 WHERE s.blue_line_key = v_blkey
 AND s.downstream_route_measure <= v_measure
-AND s.upstream_route_measure > v_measure
-INTO v_geom;
+AND s.upstream_route_measure > v_measure;
 
-RETURN v_geom;
-
-end
+END
 
 $$
-language 'plpgsql' immutable strict parallel safe;
+LANGUAGE 'PLPGSQL' IMMUTABLE STRICT PARALLEL SAFE;
 
 COMMENT ON FUNCTION postgisftw.fwa_IndexPoint IS 'Return a point on the stream network based on the location provided by blue_line_key and downstream_route_measure'
