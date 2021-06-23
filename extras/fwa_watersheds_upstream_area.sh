6f03ce6f36017545ec61f0e168efdd497a33aae0
Simon Norris <snorris@hillcrestgeo.ca>
2021-06-23 15:40:16 -0700

    do not use watershed groups as pre-aggregated units when creating watersheds_upstream_area table

--- extras/fwa_watersheds_upstream_area.sh
+++ extras/fwa_watersheds_upstream_area.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 set -euxo pipefail
 
-# create load table
+psql -c "CREATE SCHEMA if not exists temp;"
+
+# load watershed groups individually into seperate tables in temp schema
+psql -AtX -P border=0,footer=off -c "SELECT watershed_group_code
+      FROM whse_basemapping.fwa_watershed_groups_poly
+      ORDER BY watershed_group_code" |
+parallel psql -XA -v wsg={} -f sql/fwa_watersheds_upstream_area.sql
+
+# combine temp tables into output table
 psql -c "drop table if exists whse_basemapping.fwa_watersheds_upstream_area"
 psql -c "create table whse_basemapping.fwa_watersheds_upstream_area
-        (watershed_feature_id integer, upstream_area double precision);"
+        (watershed_feature_id integer primary key, upstream_area double precision);"
 
-# load watershed groups individually
-# (calling this in parallel seems to grind things to a halt)
 for WSG in $(psql -AtX -P border=0,footer=no \
   -c "SELECT watershed_group_code
       FROM whse_basemapping.fwa_watershed_groups_poly
-      ORDER BY watershed_group_code")
+      ORDER BY watershed_group_code
+      ")
 do
-  echo 'Processing '$WSG
-  psql -AX -v wsg=$WSG -f sql/fwa_watersheds_upstream_area.sql
+  echo 'Loading '$WSG
+  psql -c "INSERT INTO whse_basemapping.fwa_watersheds_upstream_area SELECT * FROM temp.fwa_watersheds_upstream_area_"$WSG
+done
+psql -c "ANALYZE whse_basemapping.fwa_watersheds_upstream_area"
+
+# drop the temp tables
+for WSG in $(psql -AtX -P border=0,footer=no \
+  -c "SELECT watershed_group_code
+      FROM whse_basemapping.fwa_watershed_groups_poly
+      ORDER BY watershed_group_code
+      ")
+do
+  psql -c "DROP TABLE temp.fwa_watersheds_upstream_area_"$WSG
 done
 
-psql -c "ALTER TABLE whse_basemapping.fwa_watersheds_upstream_area ADD PRIMARY KEY (watershed_feature_id)"
-
 echo 'All done, see whse_basemapping.fwa_watersheds_upstream_area'
-
