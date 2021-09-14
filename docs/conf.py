740c47ac06691e6289c13a1303db78dfdfd94f3b
Simon Norris <snorris@hillcrestgeo.ca>
2021-09-14 12:04:19 -0700

    add rtd theme requirement

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Simon Norris'
 author = 'Simon Norris'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.0dev'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,8 +30,7 @@ release = '0.2.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser'
-]
+extensions = ['myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
