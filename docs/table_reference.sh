231948eb42df3918497ba2cb13637a40fa0cc88b
Simon Norris <snorris@hillcrestgeo.ca>
2021-09-25 16:00:31 -0700

    clean up tableref script

--- docs/table_reference.sh
+++ docs/table_reference.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -euxo pipefail
+set -euxo pipefail
 
 # build table reference for value added tables
 echo "# Table reference" > 03_tables.md
@@ -15,12 +15,10 @@ for table in approx_borders \
 do
     echo "## whse_basemapping.fwa_$table" >> 03_tables.md
     echo -en '\n' >> 03_tables.md
-    #echo '````' >> 03_tables.md
     psql -AtX -P border=0,footer=no -c "\dt+ whse_basemapping.fwa_$table" | awk -F"|" '{print $7}' >> 03_tables.md
     echo -en '\n' >> 03_tables.md
     echo "| Column | Type | Description |" >> 03_tables.md
     echo "|--------|------|-------------|" >> 03_tables.md
     psql -AtX -P border=0,footer=no -c "\d+ whse_basemapping.fwa_$table" | awk -F"|" '{ print "| `"$1"` | `"$2"` | "$8" |"}' >> 03_tables.md
-    #echo '````' >> 03_tables.md
     echo -en '\n' >> 03_tables.md
 done
