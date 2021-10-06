8db640643f2f814eed691d20da63a810d4dc827a
Simon Norris <snorris@hillcrestgeo.ca>
2021-10-06 14:20:45 -0700

    add fixes to lut

--- sql/tables/value_added/fwa_streams_watersheds_lut.sql
+++ sql/tables/value_added/fwa_streams_watersheds_lut.sql
@@ -3,26 +3,45 @@
 -- Speed future queries by running the spatial query 
 -- for every stream, creating a lookup.
 
--- Because even using ST_PointOnSurface does not create
--- a 1:1 relationship (network connectors etc), find 
--- distinct by choosing the match with highest stream order.
--- (where the result is ambiguous it doesn't really matter
--- which watershed is chosen anyway)
-
+-- first, find matches based on watershed code
+-- where there is more than one match, match to closest
 INSERT INTO whse_basemapping.fwa_streams_watersheds_lut
 (
   linear_feature_id,
   watershed_feature_id
 )
 SELECT DISTINCT ON (linear_feature_id)
-  s.linear_feature_id, 
+  s.linear_feature_id,
   w.watershed_feature_id
 FROM whse_basemapping.fwa_stream_networks_sp s
 INNER JOIN whse_basemapping.fwa_watersheds_poly w
-ON ST_Intersects(ST_pointonsurface(s.geom), w.geom)
+ON (s.wscode_ltree = w.wscode_ltree AND
+    s.localcode_ltree = w.localcode_ltree AND
+    s.watershed_group_code = w.watershed_group_code)
 WHERE s.watershed_group_code = :'wsg'
-ORDER BY s.linear_feature_id, s.stream_order;
+AND s.fwa_watershed_code NOT LIKE '999%'
+AND s.local_watershed_code IS NOT NULL
+ORDER BY s.linear_feature_id, ST_Distance(ST_LineInterpolatePoint(s.geom, .5), ST_PointOnSurface(w.geom));
 
-COMMENT ON TABLE whse_basemapping.fwa_streams_watersheds_lut IS 'A convenience lookup for quickly relating streams and fundamental watersheds';
-COMMENT ON COLUMN whse_basemapping.fwa_streams_watersheds_lut.linear_feature_id IS 'FWA stream segment unique identifier';
-COMMENT ON COLUMN whse_basemapping.fwa_streams_watersheds_lut.watershed_feature_id IS 'FWA fundamental watershed unique identifer';
+
+-- For streams with no matching watershed based on watershed codes,
+-- do a spatial join, selecting watershed that intersects the midpoint of the stream,
+-- and where more than one watershed intersects, select the one with the closest centroid
+INSERT INTO whse_basemapping.fwa_streams_watersheds_lut
+(
+  linear_feature_id,
+  watershed_feature_id
+)
+SELECT DISTINCT ON (linear_feature_id)
+  s.linear_feature_id,
+  w.watershed_feature_id
+FROM whse_basemapping.fwa_stream_networks_sp s
+LEFT JOIN whse_basemapping.fwa_streams_watersheds_lut l
+ON s.linear_feature_id = l.linear_feature_id
+INNER JOIN whse_basemapping.fwa_watersheds_poly w
+ON ST_Intersects(ST_LineInterpolatePoint(s.geom, .5), w.geom)
+WHERE l.watershed_feature_id IS NULL -- extract only streams that are not already matched
+AND s.watershed_group_code = :'wsg'
+AND s.fwa_watershed_code NOT LIKE '999%'
+AND s.local_watershed_code IS NOT NULL
+ORDER BY s.linear_feature_id, ST_Distance(ST_LineInterpolatePoint(s.geom, .5), ST_Centroid(w.geom));
