c7a8c16a050f2a2b57ff4ef78a3fb1a1abaf4faf
Simon Norris <snorris@hillcrestgeo.ca>
2022-06-02 14:30:52 -0700

    table ref is manually edited, dump the reference to stdout for copy/paste rather than autogenerating 03_tables

--- docs/table_reference.sh
+++ docs/table_reference.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -euxo pipefail
 
 # build table reference for value added tables
-echo "# Table reference" > 03_tables.md
-echo -en '\n' >> 03_tables.md
+echo "# Table reference"
+echo -en '\n'
 
 for table in approx_borders \
   basins_poly \
@@ -12,12 +11,12 @@ for table in approx_borders \
   streams_watersheds_lut \
   waterbodies
 do
-    echo "## whse_basemapping.fwa_$table" >> 03_tables.md
-    echo -en '\n' >> 03_tables.md
-    psql -AtX -P border=0,footer=no -c "\dt+ whse_basemapping.fwa_$table" | awk -F"|" '{print $7}' >> 03_tables.md
-    echo -en '\n' >> 03_tables.md
-    echo "| Column | Type | Description |" >> 03_tables.md
-    echo "|--------|------|-------------|" >> 03_tables.md
-    psql -AtX -P border=0,footer=no -c "\d+ whse_basemapping.fwa_$table" | awk -F"|" '{ print "| `"$1"` | `"$2"` | "$8" |"}' >> 03_tables.md
-    echo -en '\n' >> 03_tables.md
+    echo "## whse_basemapping.fwa_$table"
+    echo -en '\n'
+    psql -AtX -P border=0,footer=no -c "\dt+ whse_basemapping.fwa_$table" | awk -F"|" '{print $7}'
+    echo -en '\n'
+    echo "| Column | Type | Description |"
+    echo "|--------|------|-------------|"
+    psql -AtX -P border=0,footer=no -c "\d+ whse_basemapping.fwa_$table" | awk -F"|" '{ print "| `"$1"` | `"$2"` | "$8" |"}'
+    echo -en '\n'
 done
