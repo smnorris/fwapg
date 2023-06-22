fb18d7dbb94c0c4a0b8ff643f163c11018cd9343
Simon Norris <snorris@hillcrestgeo.ca>
2023-06-22 15:43:35 -0700

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Simon Norris'
 author = 'Simon Norris'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.5.1'
 
 
 # -- General configuration ---------------------------------------------------
