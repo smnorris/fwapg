4c34f175639a3bab0a8183dc361e588bddcd4882
Simon Norris <snorris@hillcrestgeo.ca>
2024-11-19 15:46:54 -0800

    clarify

--- cache.sh
+++ cache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-# cache to s3 for (much) faster downloads
-# also, add .gdb to filename so files can be accessed directly via /vsizip//vsicurl
+# cache fwa data to s3 for (much) faster downloads
+# also, adds .gdb to filename so files can be accessed directly via /vsizip//vsicurl
 
 curl -o /tmp/FWA_BC.gdb.zip ftp://ftp.geobc.gov.bc.ca/sections/outgoing/bmgs/FWA_Public/FWA_BC.zip
 curl -o /tmp/FWA_LINEAR_BOUNDARIES_SP.gdb.zip ftp://ftp.geobc.gov.bc.ca/sections/outgoing/bmgs/FWA_Public/FWA_LINEAR_BOUNDARIES_SP.zip
