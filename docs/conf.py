1d5617bf4c279b22fe8949630120520a145447f2
Simon Norris <snorris@hillcrestgeo.ca>
2026-04-17 09:46:09 -0700

    current changes are not user facing, make it a point release

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2026, Simon Norris'
 author = 'Simon Norris'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.0'
+release = '0.7.2dev0'
 
 
 # -- General configuration ---------------------------------------------------
